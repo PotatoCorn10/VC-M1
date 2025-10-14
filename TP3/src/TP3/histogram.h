d8f4c38cbf3e0f15b8feb17d3119e32b964e7b44
Florian Linhart <flori.linhart@gmail.com>
2025-10-14 22:48:36 +0200

    continue add interface for ppm

--- TP3/src/TP3/histogram.h
+++ TP3/src/TP3/histogram.h
@@ -3,11 +3,17 @@
 
 #include "../Utils/types.h"
 
-histogram_t compute_histogram(pgm_file image);
+histogram_t compute_histogram(pgm_file);
 
-pgm_file histogram_stretching(pgm_file image);
+histogram_t compute_histogram_ppm(ppm_file);
 
-pgm_file histogram_equalization(pgm_file image);
+pgm_file histogram_stretching(pgm_file);
+
+ppm_file histogram_stretching_ppm(ppm_file);
+
+pgm_file histogram_equalization(pgm_file);
+
+ppm_file histogram_equalization_ppm(ppm_file);
 
 // Stores a histogram into a csv file format
 void save_histogram(histogram_t histogram, char *path);
